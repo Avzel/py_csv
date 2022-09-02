b16b4bf511b4486621ec384089fcae9093e9c3ed
Avzel <65988622+Avzel@users.noreply.github.com>
2022-09-02 11:37:29 +0800

    Update comments

--- py_csv/enums.py
+++ py_csv/enums.py
@@ -3,17 +3,17 @@ from enum import Enum
 
 class State(Enum):
 
-    START       = object()
-    QUOTED      = object()
-    FREE        = object()
-    END         = object()
+    START       = object()  # The start of a field
+    QUOTED      = object()  # In the middle of parsing a quoted field
+    FREE        = object()  # In the middle of parsing a non-quoted field
+    END         = object()  # At the end of the row
 
 class Type(Enum):
 
     NONE        = object()
     LF          = object()
     CR          = object()
-    SQ          = object()
-    FQ          = object()
+    SQ          = object()  # "Starting Quote" - double quote not preceded by a double quote
+    FQ          = object()  # "Following Quote" - double quote preceded by a double quote
     COMMA       = object()
-    TEXTDATA    = object()
+    TEXTDATA    = object()  # Anything else
--- py_csv/exceptions.py
+++ py_csv/exceptions.py
@@ -1,5 +1,23 @@
 class ParseException(Exception):
 
     def __init__(self, message, line, row, column, field):
+        '''
+        Prints an error message providing the line, row, column, 
+        and field numbers where the formatting error was encountered, 
+        as well as a custom given message
+        
+        Parameters
+        ----------
+        message: str
+            the custom message to be printed after the base message
+        line: int
+            the line number on which the formatting error was encountered
+        row: int
+            the row number at which the formatting error was encountered
+        column: int
+            the column number at which the formatting error was encountered
+        field: int
+            the field number in which the formatting error was encountered
+        '''
 
         print(f"Parsing error (line {line}, row {row}, column {column}, field {field}): {message}")
